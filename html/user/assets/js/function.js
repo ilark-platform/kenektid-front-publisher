cfa4d2bba335534755d187b11dd0fbbba09fa811
rnjsdhrb84 <59335661+rnjsdhrb84@users.noreply.github.com>
2022-06-22 20:47:42 +0900

    Add files via upload

--- html/user/assets/js/function.js
+++ html/user/assets/js/function.js
@@ -6,3 +6,44 @@ function setScreenSize() {
 }
 setScreenSize();
 window.addEventListener('resize', () => setScreenSize());
+
+
+
+let sbartrigger = document.querySelector('.js-sidebar-trigger');
+let sbarclose = document.querySelector('.js-sidebar-close');
+let sidebar = document.querySelector('.sidebar');
+let dimlayer = document.querySelector('.dim-layer');
+
+let classOpen = [sidebar, dimlayer];
+sbartrigger.addEventListener('click', function(e){
+  classOpen.forEach(e => e.classList.add('is-active'));
+});
+
+let classCloseClick = [dimlayer, sbarclose];
+classCloseClick.forEach(function(el) {
+  el.addEventListener('click', function(els) {
+    classOpen.forEach(els => els.classList.remove('is-active'));
+  });
+});
+
+/*===== SIDEBAR & HEADER COLLAPSE MENU  =====*/ 
+let linkCollapse = document.getElementsByClassName('collapse__link');
+for(var i=0; i<linkCollapse.length; i++){
+  linkCollapse[i].addEventListener('click', function(){
+    let collapseMenu = this.nextElementSibling
+    collapseMenu.classList.toggle('collapse__list--open')
+
+    let previousEl = collapseMenu.previousElementSibling
+    previousEl.classList.toggle('global-nav__link--open')
+  })
+}
+
+
+/* 모달 테스트용 */
+let modal = document.getElementById('modal');
+function openModal() {
+    modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";  
+}
