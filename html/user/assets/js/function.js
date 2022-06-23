910b1b4760fcd407c4751a707e8af9054a0743de
rnjsdhrb84 <59335661+rnjsdhrb84@users.noreply.github.com>
2022-06-24 01:24:03 +0900

    Add files via upload

--- html/user/assets/js/function.js
+++ html/user/assets/js/function.js
@@ -14,19 +14,22 @@ let sbarclose = document.querySelector('.js-sidebar-close');
 let sidebar = document.querySelector('.sidebar');
 let dimlayer = document.querySelector('.dim-layer');
 
-let classOpen = [sidebar, dimlayer];
-sbartrigger.addEventListener('click', function(e){
-  classOpen.forEach(e => e.classList.add('is-active'));
-});
+if(sidebar){
+  let classOpen = [sidebar, dimlayer];
 
-let classCloseClick = [dimlayer, sbarclose];
-classCloseClick.forEach(function(el) {
-  el.addEventListener('click', function(els) {
-    classOpen.forEach(els => els.classList.remove('is-active'));
+  sbartrigger.addEventListener('click', function(e){
+    classOpen.forEach(e => e.classList.add('is-active'));
   });
-});
 
-/*===== SIDEBAR & HEADER COLLAPSE MENU  =====*/ 
+  let classCloseClick = [dimlayer, sbarclose];
+  classCloseClick.forEach(function(el) {
+    el.addEventListener('click', function(els) {
+      classOpen.forEach(els => els.classList.remove('is-active'));
+    });
+  });
+}
+
+/*===== SIDEBAR & HEADER COLLAPSE MENU  =====*/
 let linkCollapse = document.getElementsByClassName('collapse__link');
 for(var i=0; i<linkCollapse.length; i++){
   linkCollapse[i].addEventListener('click', function(){
@@ -67,4 +70,36 @@ for(var i=0; i<elToggle.length; i++){
     previousEl.classList.toggle('is-open')
     
   })
+}
+
+
+/* 커스텀 셀렉트 */
+const label = document.querySelectorAll('.select-dropdown__label');
+label.forEach(function(lb){
+    lb.addEventListener('click', e => {
+        let optionList = lb.nextElementSibling;
+        let optionItems = optionList.querySelectorAll('.select-dropdown__item');
+        clickLabel(lb, optionItems);
+    })
+});
+const clickLabel = (lb, optionItems) => {
+    if(lb.parentNode.classList.contains('expanded')) {
+        lb.parentNode.classList.remove('expanded');
+        optionItems.forEach((opt) => {
+            opt.removeEventListener('click', () => {
+                handleSelect(lb, opt)
+            })
+        })
+    } else {
+        lb.parentNode.classList.add('expanded');
+        optionItems.forEach((opt) => {
+            opt.addEventListener('click', () => {
+                handleSelect(lb, opt)
+            })
+        })
+    }
+}
+const handleSelect = (label, item) => {
+    label.innerHTML = item.textContent;
+    label.parentNode.classList.remove('expanded');
 }
